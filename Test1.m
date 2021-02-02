5fbba9b9f1a871bd3dbec6f0262bc17c9de18228
James Barrett <45885505+jbsec@users.noreply.github.com>
2021-02-02 16:27:21 +0000

    Add files via upload

---
+++ Test1.m
@@ -0,0 +1,14 @@
+5 + 5 % five plus five
+3 ^ 2 % 3 rasied to the power of 2
+sin(pi /2) % sine of angle 90%
+% 7/0 seven divided by zero, not gonna run that. 
+732 * 20.3 
+x = 5;
+y = x + 5
+% apprenetly semi colons  are to supress and hide for expressions
+b = 10 % variable declaration example
+%{ 
+testing multi line comments 
+example text here 
+finish comments 
+}%
