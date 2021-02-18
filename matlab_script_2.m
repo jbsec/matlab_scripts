a6dd37dc1a12ce4b9dade9043197746cd458040d
James Barrett <45885505+jbsec@users.noreply.github.com>
2021-02-18 11:30:05 +0000

    Add files via upload
    Some variable stuff I did today

---
+++ matlab_script_2.m
@@ -0,0 +1,58 @@
+%x = 3 % defining x and initializing it with a value,3 
+%b = sqrt(16) %defining b and initialzing it with an expression, 4
+%sqrt(78); %gives 78 back, requires no variable instantiton
+%9876/ans
+%sqrt(78);
+%9876/ans
+%x = 7 * 8;
+%y = x * 7.89
+%a = 2; b = 7; c = a * b
+% below line shows all variables
+%who
+%clear % clears vairables
+%x ... will return a unrecongized function because it was cleared
+%x = 4
+%clear x
+%clear
+%x returns same error
+% long assignments
+initial_velocity = 0;
+acceleration = 9.8;
+time = 20;
+final_velocity = initial_velocity + acceleration * time
+
+format long
+x = 7 + 10/3 + 5 ^ 1.2
+
+format bank 
+daily_wage = 177.45;
+weekly_wage  = daily_wage * 6
+
+format short e %The format short e command allows displaying in exponential form with four decimal places plus the exponent.
+4.678 * 4.9
+
+clear
+
+format long e 
+x = pi
+
+% rat is for ratio
+format rat
+4.678 * 4.9
+
+%ans = 34177/1491
+
+r = [ 7 8 9 10 11 ];
+t = [ 2, 3, 4, 5, 6];
+res = r + t
+c = [7; 8; 9; 10; 11]
+m = [1 2 3; 4 5 6; 7 8 9]
+%tree = [1; 1 2; 1 2 3; 1 2 3 4; 1 2 3 4 5;] HAVE TO BE SAME LENGTH
+%MATRICES
+
+
+
+
+
+
+
